d00bfef960e2d1d33da8a63c421a2acba601e3cf
Fierflame <git@fierflame.com>
2023-05-25 13:42:32 +0800

    修复：解决无法通过 pip install -e 安装的问题

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in tianjy_organization/__init__.py
-from tianjy_organization import __version__ as version
 
 setup(
 	name="tianjy_organization",
-	version=version,
+	version='0.0.1',
 	description="天玑组织 Tianjy Organization",
 	author="天玑 Tinajy",
 	author_email="天玑 Tinajy",
