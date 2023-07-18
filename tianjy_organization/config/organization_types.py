1be20253f3f056ae75292e2ac52f93f53dc0347d
16972057@qq.com <16972057@qq.com>
2023-07-18 17:01:45 +0800

    修改：公司的父级可以是公司

--- tianjy_organization/config/organization_types.py
+++ tianjy_organization/config/organization_types.py
@@ -2,7 +2,7 @@ from frappe import _dict
 types = [
 	_dict(
 		name="Company",
-		parent_types=[],
+		parent_types=[_dict(type="Company"),],
 		leader_types=[],
 		root_only=1,
 		visible_to_descendants=1,
