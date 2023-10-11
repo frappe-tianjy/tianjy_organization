b1e5dfb4a3882a774a38d9950eb15ff50922d6f1
px <511055984@qq.com>
2023-10-11 15:37:09 +0800

    新增：新建组织菜单修改

--- tianjy_organization/tianjy_organization/page/tianjy_organization_config/tianjy_organization_config/components/TreeItem.vue
+++ tianjy_organization/tianjy_organization/page/tianjy_organization_config/tianjy_organization_config/components/TreeItem.vue
@@ -10,20 +10,21 @@
 			</span>
 			<template #dropdown>
 				<el-dropdown-menu>
-					<el-dropdown-item v-if="childTypes.length>0&&node.data.permissions.createPermission" command="add">
-						<el-popover :popper-style="{padding:0}" placement="right-start" trigger="hover" :offset="15">
-							<template #reference>
-								<span>新建子节点</span>
-							</template>
-							<el-menu class="create-menu" mode="vertical">
-								<el-menu-item class="create_type" v-for="type in childTypes" @click="createByType(type)">{{ type.name }}</el-menu-item>
-							</el-menu>
-						</el-popover>
-					</el-dropdown-item>
-					<el-dropdown-item 
-						command="delete" 
+					<template v-if="childTypes.length>0&&node.data.permissions.createPermission">
+						<el-dropdown-item
+							command="add"
+							v-for="type in childTypes"
+							@click="createByType(type)"
+						>
+							新建{{ type.name }}
+						</el-dropdown-item>
+					</template>
+					<el-dropdown-item
+						command="delete"
 						v-if="node.data.permissions.deletePermission&&!node.data.children?.length"
-					><span class="delete">删除</span></el-dropdown-item>
+					>
+						<span class="delete">删除</span>
+					</el-dropdown-item>
 				</el-dropdown-menu>
 			</template>
 		</el-dropdown>
@@ -32,10 +33,11 @@
 
 <script setup lang='ts'>
 import { ref, onMounted, computed, toRaw} from 'vue';
-import type { Organization, OrganizationType } from '../type';
-import type Node from 'element-plus/es/components/tree/src/model/node'
+import type Node from 'element-plus/es/components/tree/src/model/node';
 import { MoreFilled } from '@element-plus/icons-vue';
 import { ElMessage, ElMessageBox } from 'element-plus';
+
+import type { Organization, OrganizationType } from '../type';
 
 interface Props{
 	node:Node,
@@ -49,29 +51,29 @@ const emit = defineEmits<Emit>();
 
 async function deleteOrganization() {
 	ElMessageBox.confirm(
-			'您确认删除此组织吗?',
-			'请确认',
-			{
-				confirmButtonText: '确定',
-				cancelButtonText: '取消',
-				type: 'warning',
-			}
-		).then(async () => {
-			await frappe.db.delete_doc('Tianjy Organization', props.node.data.name)
-			emit('reload')
-			ElMessage({
-				type: 'success',
-				message: '删除成功',
-			});
-		}).catch(() => {
-			ElMessage({
-				type: 'info',
-				message: '取消删除',
-			});
+		'您确认删除此组织吗?',
+		'请确认',
+		{
+			confirmButtonText: '确定',
+			cancelButtonText: '取消',
+			type: 'warning',
+		},
+	).then(async () => {
+		await frappe.db.delete_doc('Tianjy Organization', props.node.data.name);
+		emit('reload');
+		ElMessage({
+			type: 'success',
+			message: '删除成功',
 		});
+	}).catch(() => {
+		ElMessage({
+			type: 'info',
+			message: '取消删除',
+		});
+	});
 }
 
-const childTypes = computed(()=>props.node.data.child_type_list)
+const childTypes = computed(()=>props.node.data.child_type_list);
 function command(v: string) {
 	switch (v) {
 		case 'delete': return deleteOrganization();
@@ -91,7 +93,7 @@ function createByType(type:OrganizationType){
 	color: #f00;
 }
 .create_type{
-	height: 24px;	
+	height: 24px;
 }
 .create-menu{
 	border-right: 0;
