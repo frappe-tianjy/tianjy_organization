24c765313911ec05af7d9fac3ac8ba9e6077a078
px <511055984@qq.com>
2023-10-13 17:11:04 +0800

    新增：组织类型更改为不能设置为默认时，取掉所有改类型的默认组织

--- tianjy_organization/tianjy_organization/doctype/tianjy_organization_type/tianjy_organization_type.py
+++ tianjy_organization/tianjy_organization/doctype/tianjy_organization_type/tianjy_organization_type.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2023, 天玑 Tinajy and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.query_builder.utils import DocType
+
 
 class TianjyOrganizationType(Document):
-	DOCTYPE='Tianjy Organization Type'
-	pass
+    DOCTYPE = 'Tianjy Organization Type'
+
+    def on_update(self):
+        if not self.no_default:
+            return
+        org_list = frappe.get_all('Tianjy Organization', filters=[
+            ['type', '=', self.name]
+        ])
+        Table = DocType('Tianjy Organization Member')
+        frappe.qb.update(Table).set(Table.default, 0).where(
+            Table.organization.isin(list(map(lambda ol: ol.name, org_list)))
+        ).run()
