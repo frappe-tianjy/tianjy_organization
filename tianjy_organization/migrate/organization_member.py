e70e66bf5d1faed2cfaf3ed51c1134b43b67d6e9
步贺 <1140437044@qq.com>
2023-10-17 13:28:02 +0800

    新增：inherit 迁移

--- tianjy_organization/migrate/organization_member.py
+++ tianjy_organization/migrate/organization_member.py
@@ -4,7 +4,7 @@ import frappe
 
 def run():
     Table = DocType('Tianjy Organization Member')
-    q = frappe.qb.update(Table).where(Table.is_inherit == '')
+    q = frappe.qb.update(Table).where(Table.is_inherit.isnull())
     q = q.set(Table['is_inherit'], 0)
     q = q.set(Table['inherit_from'], Table.organization)
     q.run()
